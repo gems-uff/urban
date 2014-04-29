366ee61c3bdf8a03f05469045e28b9fbab256fad
Bruno Schettino <bschettino@id.uff.br>
2014-04-29 11:58:16 -0300

    Adding empty DBConnection class

---
+++ Extractor/src/br/uff/bus_data/helper/DBConnection.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.uff.bus_data.helper;
+
+/**
+ *
+ * @author schettino
+ */
+public class DBConnection {
+    public static final String JDBC = "";
+    public static final String DATABASE = "";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
+    
+}
